e3bcdf37f300b06c3b68b183d28019222c554a5e
krzysztofhejna <khejna15@wp.pl>
2019-02-17 19:25:28 +0100

    Use PUG template in app

--- server.js
+++ server.js
@@ -4,20 +4,19 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views','./views');
 
-app.use('/store', function(req, res, next){
-  console.log('This is a middleware on a /store request');
-  next();
-});
-
 app.get('/', function (req, res) {
-  res.send('Hello world!');
+  res.render('index');
 });
 
-app.get('/store', function (req, res) {
-  res.send('This is the shop!');
+app.get('/auth/google', function(req, res) {
+  res.render('auth', {
+    user: {
+      name: "Krzysztof"
+    }
+  });
 });
 
 app.listen(3000);
 app.use(function (req, res, next) {
-  res.status(404).send("I'm sorry, we coudln't find what you were looking for!")
+  res.status(404).send("I'm sorry, we couldn't find what you were looking for!")
 });
